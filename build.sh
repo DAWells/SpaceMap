6773e88501c9874ea12a7843736db356e4871911
DAWells <david.arthur.wells@gmail.com>
2026-01-05 22:20:26 +0000

    add needed netlify files

---
+++ build.sh
@@ -0,0 +1,13 @@
+echo "Download solar system"
+python -m plot.solarWindRose.download_solar_system
+echo "Preprocess solar system"
+python -m plot.solarWindRose.preprocess_solar_system
+echo "Plot solar system"
+python -m plot.solarWindRose.plot_solar_system
+
+mkdir public
+cp data/images/* public
+cp webpage/completetemplate.html public
+cp webpage/style.css public
+
+echo "Build complete"
